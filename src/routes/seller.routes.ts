963178c4498c0e164a412033763e4696624aecce
Sharif-11 <sharifsharifulislam061@gmail.com>
2025-02-05 21:54:40 +0600

    referral route added

--- src/routes/seller.routes.ts
+++ src/routes/seller.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
+import authControllers from '../controllers/auth.controllers'
 import { isAuthenticated, verifySeller } from '../middlewares/auth.middlewares'
 import sellerProductsRouter from './sellerProducts.route'
 import sellerWithdrawRouter from './sellerWithdraw.routes'
 import walletRouter from './wallet.routes'
 const sellerRouter = Router()
 sellerRouter.use(isAuthenticated, verifySeller)
+sellerRouter.post('/add-referral', authControllers.addReferralCode)
 sellerRouter.use('/products', sellerProductsRouter)
 sellerRouter.use('/wallets', walletRouter)
 sellerRouter.use('/withdraw', sellerWithdrawRouter)
+
 export default sellerRouter
